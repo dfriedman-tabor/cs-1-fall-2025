b892133c1ac660c491d1db0442a113c2b5d7a710
dfriedman-tabor <dfriedman@taboracademy.org>
2025-09-04 10:46:17 -0400

    Add files via upload

---
+++ intro/SA1_Example.py
@@ -0,0 +1,24 @@
+# david friedman
+# homework 1 example
+# help from mr voci, cgpt, femi
+
+answer1 = input("what is the biggest planet? ")
+
+if answer1 == "jupiter":
+    print("correct!")
+else:
+    print("nope. the answer is jupiter")
+
+answer2 = input("what team will win the super bowl?")
+
+if answer2 == "eagles":
+    print("correct!")
+else:
+    print("nope. go birds")
+
+
+
+
+
+
+
