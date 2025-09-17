fa79d7ca26885733787ef108bbd5c4e9faa83a2e
dfriedman-tabor <dfriedman@taboracademy.org>
2025-09-17 09:26:19 -0400

    Update BasicGraphicsFiller.py

--- graphics/BasicGraphicsFiller.py
+++ graphics/BasicGraphicsFiller.py
@@ -2,19 +2,23 @@
 
 
 # dimensions of the window
-width = 600
-height = 800
+width = 800
+height = 600
 
 def draw(canvas):
+    canvas.fill((255, 255, 255))
 
-    pygame.draw.rect(canvas, (255, 0, 0), (0, 50, 600, 50))
-    pygame.draw.rect(canvas, (255, 0, 0), (300, 300, 50, 50))
+    # draws a circle at location (300, 100), with radius 100
+    pygame.draw.circle(canvas, (255,0,0), (300, 100), 100)
 
-    pygame.draw.circle(canvas, (0, 255, 0), (300, 300), 40)
+    # draws a rectangle at location (200, 40), with width of 50 and height of 75
+    pygame.draw.rect(canvas, (0,0,255), (200, 40, 50, 75))
 
+    # draws a line that connects points (100, 100) and (300, 300)
+    pygame.draw.line(canvas, (255, 0, 255), (100, 100), (300, 300), 3)
 
-
-
+    # draws a triangle connecting the three points listed
+    pygame.draw.polygon(canvas, (0, 0, 255), [(200, 200), (500, 300), (300, 500)])
 
 
 
@@ -37,22 +41,5 @@ while True:
             pygame.quit()
             sys.exit()
     pygame.display.update()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
